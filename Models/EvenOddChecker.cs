650671151225ec0d0f4eaaf8eb72949acd6b4001
Mantvydas Šemeta <mantvydassemeta@gmail.com>
2024-07-29 08:42:55 +0300

    Update

--- Models/EvenOddChecker.cs
+++ Models/EvenOddChecker.cs
@@ -13,7 +13,7 @@ namespace Interfeisai.Models
         {
             for (int i = 1; i <= 20; i++)
             {
-                if (i % 2 == 1)
+                if (i % 2 == 0)
                 {
 
                     Console.WriteLine($"{i} skaicius yra lyginis");
